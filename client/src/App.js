89c1792bbc7ad6d2867e3152055fadb1eb2a1f47
Roni Maskey <ronimaskey@gmail.com>
2020-10-27 10:44:45 +0100

    [created] store function

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
+  <Provider store={store}>
   <Router>
     <Fragment>
       <Navbar />
@@ -20,5 +24,6 @@ const App = () => (
       </section>
     </Fragment>
   </Router>
+  </Provider>
 );
 export default App;
