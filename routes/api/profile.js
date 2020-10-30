e31f4d850c69f80b1e3dcb6e06e160c977d53658
Roni Maskey <ronimaskey@gmail.com>
2020-10-30 15:07:37 +0100

    [added]profile reducer/[get]current profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,17 +13,15 @@ const Profile = require('../../models/Profile');
 // @access Private
 router.get('/me', auth, async (req, res) => {
     try {
-        const profile = await Profile.findOne({
-            user: req.user.id
-        }).populate(
+        const profile = await Profile.findOne({ user: req.user.id }).populate(
             'user',
             ['name', 'avatar']
         );
+
         if (!profile) {
-            return res.status(400).json({
-                msg: 'No profile picture'
-            });
+            return res.status(400).json({ msg: 'No profile picture' });
         }
+        
         res.json(profile);
     } catch (err) {
         console.error(err.message);
